262b8a62e3e8b522a31adadf22cbc78627b0b0de
Ranim JADID <ranimjadid@gmail.com>
2025-06-26 18:37:12 +0300

    Initial commit

---
+++ PhoneDirectory.Core/Entities/Kisi.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhoneDirectory.Core.Entities
+{
+    public class Kisi
+    {
+        public int Id { get; set; }
+        public required string Ad { get; set; }
+        public required string Soyad { get; set; }
+        public required string Telefon { get; set; }
+        public required string Email { get; set; }
+    }
+}
+
